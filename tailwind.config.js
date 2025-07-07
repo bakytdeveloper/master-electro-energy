7a926e426f547c5ddbc8401e091aaf87100d6765
bakytdeveloper <bakytdeveloper@gmail.com>
2025-07-07 14:36:11 +0600

    Изменил цвета заголовков с желтого, на синий

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
           200: "#fde68a",
           300: "#fcd34d",
           400: "#fbbf24",
-          500: "#046ddd",
+          500: "#0541f5",
         },
         beige: "#f5f5dc",
         sand: "#fdf6e3",
