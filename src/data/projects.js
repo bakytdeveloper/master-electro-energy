d8745d8380534bbb11bd217d7931ce58528041a2
bakytdeveloper <bakytdeveloper@gmail.com>
2025-07-13 15:59:34 +0600

    Перенес все картинки в src

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,27 @@
+import powerline from "../assets/projects/powerline.png";
+import substation from "../assets/projects/substation.png";
+import testing from "../assets/projects/testing.png";
+
 const projects = [
     {
         id: 1,
         title: "Монтаж подстанции 110 кВ",
         category: "Подстанции",
-        image: "/images/projects/substation.jpg",
+        image: substation,
         description: "Полный цикл монтажа и наладки оборудования подстанции.",
     },
     {
         id: 2,
         title: "Строительство ЛЭП 35 кВ",
         category: "ЛЭП",
-        image: "/images/projects/powerline.jpg",
+        image: powerline,
         description: "Возведение линии электропередач в горной местности.",
     },
     {
         id: 3,
         title: "Испытания трансформаторов",
         category: "Испытания",
-        image: "/images/projects/testing.jpg",
+        image: testing,
         description: "Лабораторные испытания и диагностика оборудования.",
     },
 ];
