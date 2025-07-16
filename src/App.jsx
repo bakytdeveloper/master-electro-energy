babd7a4861f3bb4f45e297bbdb537f5fff426cf2
bakytdeveloper <bakytdeveloper@gmail.com>
2025-07-16 18:01:33 +0600

    Обновил коды, для лучшей читаймости

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
-// import Services from "./pages/Services";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
