cdaca3aea3b98d9677e72765d76ae2f357343aff
bakytdeveloper <bakytdeveloper@gmail.com>
2025-08-01 00:37:23 +0600

    Начал настройку, чтобы код можно было запустить через гитхаб, можно удалить этот коммит, код не сломается

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
                 <Header />
                 <main className="flex-grow">
                     <Routes>
-                        <Route path="/master-electro-energy" element={<Home />} />
+                        <Route path="/" element={<Home />} />
                         <Route path="/services" element={<Services />} />
                         <Route path="/projects" element={<Projects />} />
                         <Route path="/about" element={<About />} />
